85cdde6ffc6715ebd4d4f9acb456503ee41beac8
Anes Brotlić <anes.brotlic@edu.fit.ba>
2019-08-12 14:45:45 +0200

    config final commit

--- MovieApp/Web/src/environments/environment.prod.ts
+++ MovieApp/Web/src/environments/environment.prod.ts
@@ -1,3 +1,24 @@
+let _webAPIBaseURL = 'http://localhost:44322';
+let actorURL = `${_webAPIBaseURL}/api/Actor`;
+let movieURL = `${_webAPIBaseURL}/api/Movie`;
+let authURL = `${_webAPIBaseURL}/api/Auth`;
+
 export const environment = {
-  production: true
+  production: true,
+
+  webAPIBaseURL: _webAPIBaseURL,
+
+  // MOVIE URLs
+  moviesURL: `${movieURL}`,
+  rateMovieByIdURL: `${movieURL}/rate`,
+
+  // AUTH URLs
+  loginURL: `${authURL}/login`,
+
+  // ACTOR URLs
+  actorsURL: `${actorURL}`,
+
+  noPhotoPath: 'assets/no-photo.png',
+  loaderGifPath: 'assets/loader.gif',
+
 };
